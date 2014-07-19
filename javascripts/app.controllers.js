89bfd131b65ff60bfa9a468ad08738b66a01c9bb
Asvarduil <andrew.russel.gray@gmail.com>
2014-07-19 11:39:38 -0500

    Fixed typo in a link

---
+++ javascripts/app.controllers.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var appControllers = angular.module('app.controllers', []);
+
+appControllers.controller('HomeController', [
+	function () {
+	}
+]);
+
+appControllers.controller('BlogController', [
+	function () {
+	}
+]);
+
+appControllers.controller('WebplayerController', [
+	function () {
+	}
+]);
---
+++ javascripts/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var app = angular.module('app', [
+	'ngRoute',
+	'app.controllers'
+]);
+
+app.config(['$routeProvider', 
+	function ($routeProvider) {
+		$routeProvider.when('/home', {
+			templateUrl: 'partials/home.html',
+			controller: 'HomeController'
+		});
+		$routeProvider.when('/blog', {
+			templateUrl: 'partials/blog.html',
+			controller: 'BlogController'
+		});
+		$routeProvider.when('/webplayer', {
+			templateUrl: 'partials/webplayer.html',
+			controller: 'WebplayerController'
+		});
+		
+		$routeProvider.otherwise({
+			redirectTo: '/home'
+		});
+	}
+]);
