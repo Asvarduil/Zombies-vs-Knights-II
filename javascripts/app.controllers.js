9621070da987c0d7b4640d94773d55e59cb5a845
Andrew R. Gray <andrew.russel.gray@gmail.com>
2014-08-29 14:19:15 -0500

    Update app.controllers.js

--- javascripts/app.controllers.js
+++ javascripts/app.controllers.js
@@ -13,19 +13,34 @@ appControllers.controller('HomeController', [
 appControllers.controller('BlogController', [
 	'$scope', '$http', '$sce',
 	function ($scope, $http, $sce) {
+		// Private functions...
+		function successfulContentLoad(response) {
+			$scope.entries = response.data; 
+		}
+		
+		function failedContentLoad(response) {
+			$scope.entries = [
+				{
+					"title": "Content failed to load",
+					"content": "Something went wrong, but never fear!  Reload the page or try again later."
+				}
+				}
+			];
+		}
+		
+		// Public functions...
+		$scope.parseEntry = function(entry) {
+			return $sce.trustAsHtml(entry);
+		}
+		
+		// On Load...
 		var dataPromise = $http({
 			method: 'GET',
 			url: 'content/blog-entries.json',
 			responseType: 'text'
 		});
 		
-		dataPromise.then(function(result){
-			$scope.entries = result.data; 
-		});
-		
-		$scope.parseEntry = function(entry) {
-			return $sce.trustAsHtml(entry);
-		}
+		dataPromise.then(successfulContentLoad, failedContentLoad);
 	}
 ]);
 
